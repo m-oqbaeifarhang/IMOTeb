3dded293824690ef2d5fb918b6b0a8016ceca764
mohammad <m.oqbaeifarhang@gmail.com>
2020-03-24 20:11:32 +0430

    chetori

--- app/src/main/java/com/example/imoteb/Test_mezaj_resultFragment.kt
+++ app/src/main/java/com/example/imoteb/Test_mezaj_resultFragment.kt
@@ -22,6 +22,7 @@ class Test_mezaj_resultFragment : Fragment()
     private var param1: String? = null
     private var param2: String? = null
 
+    
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
