f4d5429a2fe427aff71969cc4da32086059fd36b
mohammad <m.oqbaeifarhang@gmail.com>
2020-05-18 10:55:59 +0430

    Ala

---
+++ app/src/main/java/com/example/imoteb/Globals.kt
@@ -0,0 +1,11 @@
+package com.example.imoteb
+
+
+class Globals
+{
+    companion object
+    {
+        public enum class MezajhaEnum{dam,safra,soda,balgham}
+    }
+
+}
---
+++ app/src/main/java/com/example/imoteb/Mohasebeh_GhalabeHa.kt
@@ -0,0 +1,36 @@
+package com.example.imoteb
+import com.example.imoteb.Globals.Companion.MezajhaEnum
+
+import kotlinx.coroutines.GlobalScope
+
+enum class MezajehGhalebehEnum
+{
+    GhalabehKamDam, GhalabehKamsafra, GhalabehKamsoda, GhalabehKambalgham, GhalabehMotavasetDam, GhalabehMotavasetsafra, GhalabehMotavasetsoda, GhalabehMotavasetbalgham, GhalabehShadidDam, GhalabehShadidsafra, GhalabehShadidsoda, GhalabehShadidbalgham, Etedal
+
+}
+
+
+class Mohasebeh_GhalabeHa
+{
+
+//    fun Mohasebe(dam: Float, safra: Float, soda: Float, balgham: Float): MezajehGhalebehEnum
+//    {
+//        val list = mutableMapOf<MezajhaEnum,Float>()
+//        list[MezajhaEnum.dam] = dam
+//        list[MezajhaEnum.safra] = safra
+//        list[MezajhaEnum.soda] = soda
+//        list[MezajhaEnum.balgham] = balgham
+
+//        list.toSortedMap()
+//        list.filterValues {
+//            
+//        }
+//        if()
+//        {
+//
+//        }
+
+   // }
+
+
+}
