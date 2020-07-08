8288b71c5e55439b8933e7749ac78c4cf003fcf9
mohammad <m.oqbaeifarhang@gmail.com>
2020-07-08 11:07:12 +0430

    Last

---
+++ app/src/main/java/com/example/imoteb/MyAplication.kt
@@ -0,0 +1,20 @@
+package com.example.imoteb
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class MyAplication: Application()
+{
+    override fun onCreate()
+    {
+        super.onCreate()
+        Realm.init(this)
+        var realmConfiguration:RealmConfiguration=RealmConfiguration.Builder()
+            .name("IMOTebDb.realm")
+            .schemaVersion(1)
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(realmConfiguration)
+    }
+}
