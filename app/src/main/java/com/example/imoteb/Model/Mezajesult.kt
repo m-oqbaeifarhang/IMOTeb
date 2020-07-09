1647f95a799ef271a8e2efa7ecc14deb6eec1b4b
mohammad <m.oqbaeifarhang@gmail.com>
2020-07-09 09:43:43 +0430

    Last

--- app/src/main/java/com/example/imoteb/Model/Mezajesult.kt
+++ app/src/main/java/com/example/imoteb/Model/Mezajesult.kt
@@ -1,4 +1,14 @@
 package com.example.imoteb.Model
 
-class Mezajesult
-{}
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+class Mezajesult : RealmObject()
+{
+    var Dam: Double = 0.0
+    var Safra: Double = 0.0
+    var Soda: Double = 0.0
+    var Balgham: Double = 0.0
+
+
+}
