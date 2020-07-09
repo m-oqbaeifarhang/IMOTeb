b8971b67ae704b67688f63a60920ecee6b58aab5
mohammad <m.oqbaeifarhang@gmail.com>
2020-07-09 10:43:39 +0430

    Last

--- app/src/main/java/com/example/imoteb/Model/MezajResultDAO.kt
+++ app/src/main/java/com/example/imoteb/Model/MezajResultDAO.kt
@@ -1,14 +1,14 @@
 package com.example.imoteb.Model
 import io.realm.Realm
-class MezajResultDAO
-{
-private  var realm:Realm
-    init
-    {
-        realm=Realm.getDefaultInstance()
-    }
-    fun save(mezajesult: Mezajesult):Unit
-    {
-        
-    }
-}
+//class MezajResultDAO
+//{
+//private  var realm:Realm
+//    init
+//    {
+//        realm=Realm.getDefaultInstance()
+//    }
+//    fun save(mezajesult: Mezajesult):Unit
+//    {
+//
+//    }
+//}
