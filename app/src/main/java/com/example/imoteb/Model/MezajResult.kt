3f70ee71bde0e18364afabcc297be611f885bb73
mohammad <m.oqbaeifarhang@gmail.com>
2020-07-10 05:42:15 +0430

    Last

---
+++ app/src/main/java/com/example/imoteb/Model/MezajResult.kt
@@ -0,0 +1,9 @@
+package com.example.imoteb.Model
+
+import io.realm.RealmObject
+
+ open class MezajResult(var Dam: Double, var Safra: Double, var Soda: Double, var Balgham: Double) :
+    RealmObject()
+{
+
+}
