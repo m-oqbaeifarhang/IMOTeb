56cb89c6683fcfd21f3ebf82738e5e13cb2ae401
mohammad <m.oqbaeifarhang@gmail.com>
2020-07-11 10:30:22 +0430

    Last

--- app/src/main/java/com/example/imoteb/Model/MezajResult.kt
+++ app/src/main/java/com/example/imoteb/Model/MezajResult.kt
@@ -2,8 +2,10 @@ package com.example.imoteb.Model
 
 import io.realm.RealmObject
 
- open class MezajResult(var Dam: Double, var Safra: Double, var Soda: Double, var Balgham: Double) :
+ open class MezajResult(var Dam: Double=0.0, var Safra: Double=0.0, var Soda: Double=0.0, var Balgham: Double=0.0) :
     RealmObject()
-{
+ {
+
+
 
 }
